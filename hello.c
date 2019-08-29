f5d9b745b5cb2400ccff661a4ecfce4ca0e66cb7
emartinez11huskers <54558033+emartinez11huskers@users.noreply.github.com>
2019-08-29 17:20:44 -0500

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Eduardo Martine
+ * Date: 2019/8/29
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Eduardo Martinez \n");
+
+  return 0;
+}
